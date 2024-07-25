bcff0fe4fe6583ba0fe86f99b513273aed585404
olucasortiz <lucassas@outlook.com>
2024-07-25 11:42:07 -0300

    StringBuilder Final

---
+++ Demo StringBuilder/Entities/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_StringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> comments { get; set; } = new List<Comment>();
+
+        public Post() { }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        public void AddComment(Comment comment)
+        {
+            comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Coments: ");
+            foreach (Comment comment in comments)
+            {
+                sb.AppendLine(comment.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Demo StringBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using Demo_StringBuilder.Entities;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("have a nice trip");
+            Comment c2 = new Comment("wow thats awesome");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"), "Traveling to Porto Seguro", "I'm going to visit this city", 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+            Console.WriteLine(p1);
+        }
+    }
+}
